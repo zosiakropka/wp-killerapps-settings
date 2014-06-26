5f72a30888eab8f45732f9fb9c8845b390b29152
Zosia Sobocińska <sobocinska.zosia@gmail.com>
2014-06-26 12:19:28 +0200

    Set image input val to current val

--- js/settings.js
+++ js/settings.js
@@ -1,101 +1,105 @@
+
 // Image
 (function($) {
 
-	detach_buttons = jQuery('.killerapps-settings-image-detach');
-	detach_buttons.live('click', function(event) {
-		var detach_button = jQuery(event.currentTarget);
-		var input = jQuery(detach_button).siblings('input[type="hidden"]');
-		var image_container = jQuery(detach_button).siblings('.killerapps-settings-image-container');
+  detach_buttons = jQuery('.killerapps-settings-image-detach');
+  detach_buttons.live('click', function(event) {
+    var detach_button = jQuery(event.currentTarget);
+    var input = jQuery(detach_button).parent().siblings('input[type="hidden"]');
+    var image_container = jQuery(detach_button).parent('.killerapps-settings-image-container');
 
-		input.val('');
-		image_container.html('');
-		detach_button.addClass("hidden");
-	});
+    var input_val = $.parseJSON(input.val());
+    input_val.remove()
 
-	function initGallery() {
+    input.val($toJSON(input_val));
+    image_container.html('');
+    detach_button.addClass("hidden");
+  });
 
-		if ( typeof wp === 'undefined') {
+  function initGallery() {
 
-			setTimeout(initGallery, 1000);
+    if (typeof wp === 'undefined') {
 
-		} else {
+      setTimeout(initGallery, 1000);
 
-			// Uploading files
-			var file_frame;
-			var wp_media_post_id = wp.media.model.settings.post.id;
-			// Store the old id
-			var set_to_post_id = 10;
-			// Set this
-			var select_buttons = jQuery('.killerapps-settings-image-select');
+    } else {
 
-			select_buttons.live('click', function(event) {
+      // Uploading files
+      var file_frame;
+      var wp_media_post_id = wp.media.model.settings.post.id;
+      // Store the old id
+      var set_to_post_id = 10;
+      // Set this
+      var select_buttons = jQuery('.killerapps-settings-image-select');
 
-				var select_button = event.currentTarget;
-				var input = jQuery(select_button).prev('input');
-				var image_container = jQuery(select_button).next('.killerapps-settings-image-container');
-				var detach_button = jQuery(select_button).siblings('.killerapps-settings-image-detach');
+      select_buttons.live('click', function(event) {
 
-				event.preventDefault();
+        var select_button = event.currentTarget;
+        var input = jQuery(select_button).prev('input');
+        var image_container = jQuery(select_button).next('.killerapps-settings-image-container');
+        var detach_button = jQuery(select_button).siblings('.killerapps-settings-image-detach');
 
-				// If the media frame already exists, reopen it.
-				if (file_frame) {
-					// Set the post ID to what we want
-					file_frame.uploader.uploader.param('post_id', set_to_post_id);
-					// Open frame
-					file_frame.open();
-					return;
-				} else {
-					// Set the wp.media post id so the uploader grabs the ID we want when initialised
-					wp.media.model.settings.post.id = set_to_post_id;
-				}
+        event.preventDefault();
 
-				// Create the media frame.
-				file_frame = wp.media.frames.file_frame = wp.media({
-					title : jQuery(this).data('uploader_title'),
-					button : {
-						text : jQuery(this).data('uploader_button_text'),
-					},
-					multiple : false // Set to true to allow multiple files to be selected
-				});
+        // If the media frame already exists, reopen it.
+        if (file_frame) {
+          // Set the post ID to what we want
+          file_frame.uploader.uploader.param('post_id', set_to_post_id);
+          // Open frame
+          file_frame.open();
+          return;
+        } else {
+          // Set the wp.media post id so the uploader grabs the ID we want when initialised
+          wp.media.model.settings.post.id = set_to_post_id;
+        }
 
-				// When an image is selected, run a callback.
-				file_frame.on('select', function() {
-					// We set multiple to false so only get one image from the uploader
-					attachment = file_frame.state().get('selection').first().toJSON();
+        // Create the media frame.
+        file_frame = wp.media.frames.file_frame = wp.media({
+          title: jQuery(this).data('uploader_title'),
+          button: {
+            text: jQuery(this).data('uploader_button_text'),
+          },
+          multiple: false // Set to true to allow multiple files to be selected
+        });
 
-					input.val(attachment.id);
-					image_container.html("<img src='" + attachment.url + "'/>");
-					detach_button.removeClass("hidden");
+        // When an image is selected, run a callback.
+        file_frame.on('select', function() {
+          // We set multiple to false so only get one image from the uploader
+          attachment = file_frame.state().get('selection').first().toJSON();
 
-					// Do something with attachment.id and/or attachment.url here
+          input.val(attachment.id);
+          image_container.html("<img src='" + attachment.url + "'/>");
+          detach_button.removeClass("hidden");
 
-					// Restore the main post ID
-					wp.media.model.killerapps_settings.post.id = wp_media_post_id;
-				});
+          // Do something with attachment.id and/or attachment.url here
 
-				// Finally, open the modal
-				file_frame.open();
-			});
+          // Restore the main post ID
+          wp.media.model.killerapps_settings.post.id = wp_media_post_id;
+        });
 
-			// Restore the main ID when the add media button is pressed
-			jQuery('a.add_media').on('click', function() {
-				wp.media.model.killerapps_settings.post.id = wp_media_post_id;
-			});
+        // Finally, open the modal
+        file_frame.open();
+      });
 
-		}
-	}
+      // Restore the main ID when the add media button is pressed
+      jQuery('a.add_media').on('click', function() {
+        wp.media.model.killerapps_settings.post.id = wp_media_post_id;
+      });
 
-	initGallery();
+    }
+  }
+
+  initGallery();
 
 })(jQuery);
 
 
 // Range
 (function($) {
-	$(document).ready(function() {
-		$('.killerapps-settings-range').on('change', function(e) {
-			$(e.target).siblings('.killerapps-settings-range-value').html(e.target.value);
-		})
-	});
+  $(document).ready(function() {
+    $('.killerapps-settings-range').on('change', function(e) {
+      $(e.target).siblings('.killerapps-settings-range-value').html(e.target.value);
+    })
+  });
 })(jQuery);
 
